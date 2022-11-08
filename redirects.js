3a1678f9b2ca9e8132044f7ad1517019fd0cdf82
Code + Tonic <90833903+CodeAndTonicHQ@users.noreply.github.com>
2022-11-08 21:58:13 +0200

    Create redirects.js

---
+++ redirects.js
@@ -0,0 +1,103 @@
+redirectAppointmentType("Human Design Basic Reading mit Monique", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-monique/payment?pid=63077&prid=9537"); 
+
+redirectAppointmentType("Human Design Basic Reading mit Maike", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-maike/payment?pid=63076&prid=9536"); 
+
+redirectAppointmentType("Human Design Basic Reading mit Wiebke", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-wiebke/payment?pid=63086&prid=9535");
+
+redirectAppointmentType("Human Design Basic Reading mit Nadine Sachse", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-nadine/payment?pid=62465&prid=9534");
+
+redirectAppointmentType("Human Design Basic Reading mit Simone", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-simone/payment?pid=63085&prid=9533");
+
+redirectAppointmentType("Human Design Basic Reading mit Alex", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-alex/payment?pid=63082&prid=9532");
+
+redirectAppointmentType("Human Design Basic Reading mit Kristin", "https://elopage.com/epl/EWVnk-SnpRNB-dBSKTNp");
+
+redirectAppointmentType("Human Design Basic Reading mit Uli", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-uli/payment?pid=63631&prid=9628");
+
+redirectAppointmentType("Human Design Basic Reading mit Katharina", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-katharina/payment?pid=66698&prid=10051");
+
+redirectAppointmentType("Human Design Basic Reading mit Nina", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-nina/payment?pid=66699&prid=10052");
+
+redirectAppointmentType("Human Design Basic Reading mit Janine", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-janine/payment?pid=66703&prid=10053");
+
+redirectAppointmentType("Human Design Basic Reading mit Janina", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-janina/payment?pid=66716&prid=10054");
+
+redirectAppointmentType("Human Design Basic Reading mit Sabine", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-sabine/payment?pid=66711&prid=10055");
+
+redirectAppointmentType("Human Design Basic Reading mit Katha", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-katha/payment?pid=66701&prid=10056");
+
+redirectAppointmentType("Human Design Basic Reading mit Niklas", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/basic-reading-mit-niklas/payment?pid=66714&prid=10057");
+
+redirectAppointmentType("Human Design Next Step Reading mit Monique", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-monique/payment?pid=63077&prid=9537");
+  
+redirectAppointmentType("Human Design Next Step Reading mit Maike", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-maike/payment?pid=63076&prid=9536");
+
+redirectAppointmentType("Human Design Next Step Reading mit Wiebke", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-wiebke/payment?pid=63086&prid=9535");
+
+redirectAppointmentType("Human Design Next Step Reading mit Nadine Sachse", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-nadine/payment?pid=62465&prid=9534");
+
+redirectAppointmentType("Human Design Next Step Reading mit Simone", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-simone/payment?pid=63085&prid=9533");
+
+redirectAppointmentType("Human Design Next Step Reading mit Alex", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-alex/payment?pid=63082&prid=9532");
+
+redirectAppointmentType("Human Design Next Step Reading mit Kristin", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-kristin/payment?pid=31516&prid=9499");
+
+redirectAppointmentType("Human Design Next Step Reading mit Uli", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-uli/payment?pid=63631&prid=9628");
+
+redirectAppointmentType("Human Design Next Step Reading mit Katharina", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-katharina/payment?pid=66698&prid=10051");
+
+redirectAppointmentType("Human Design Next Step Reading mit Nina", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-nina/payment?pid=66699&prid=10052");
+
+redirectAppointmentType("Human Design Next Step Reading mit Janine", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-janine/payment?pid=66703&prid=10053");
+
+redirectAppointmentType("Human Design Next Step Reading mit Janina", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-janina/payment?pid=66716&prid=10054");
+
+redirectAppointmentType("Human Design Next Step Reading mit Sabine", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-sabine/payment?pid=66711&prid=10055");
+
+redirectAppointmentType("Human Design Next Step Reading mit Katha", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-katha/payment?pid=66701&prid=10056");
+
+redirectAppointmentType("Human Design Next Step Reading mit Niklas", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/next-step-reading-mit-niklas/payment?pid=66714&prid=10057");
+
+redirectAppointmentType("Familien Basic Reading mit Maike", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/familien-reading-mit-maike/payment?pid=63076&prid=9536");
+
+redirectAppointmentType("Familien Basic Reading mit Wiebke", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/familien-basic-reading-mit-wiebke/payment?pid=63086&prid=9535");
+
+redirectAppointmentType("Familien Basic Reading mit Alex", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/familien-basic-reading-mit-alex/payment?pid=63082&prid=9532");
+
+redirectAppointmentType("Familien Basic Reading mit Uli", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/familien-reading-mit-uli/payment?pid=63631&prid=9628");
+
+redirectAppointmentType("Familien Deep Dive Reading mit Maike", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/familien-penta-reading-mit-maike/payment?pid=63076&prid=9536");
+
+redirectAppointmentType("Familien Deep Dive Reading mit Wiebke", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/familien-penta-reading-mit-wiebke/payment?pid=63086&prid=9535");
+
+redirectAppointmentType("Familien Deep Dive Reading mit Alex", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/familien-penta-reading-mit-alex/payment?pid=63082&prid=9532");
+
+redirectAppointmentType("Partner Interaktions-Reading mit Wiebke", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/interaktions-reading-mit-wiebke/payment?pid=63086&prid=9535");
+
+redirectAppointmentType("Partner Interaktions-Reading mit Nadine Sachse", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/interaktions-reading-mit-nadine/payment?pid=62465&prid=9534");
+
+redirectAppointmentType("Partner Interaktions-Reading mit Alex", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/interaktions-reading-mit-alex/payment?pid=63082&prid=9532");
+
+redirectAppointmentType("Partner Bundle Interaktions-Reading mit Wiebke", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/partner-bundle-mit-wiebke/payment?pid=63086&prid=9535");
+
+redirectAppointmentType("Partner Bundle Interaktions-Reading mit Nadine Sachse", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/partner-bundle-mit-nadine/payment?pid=62465&prid=9534");
+
+redirectAppointmentType("Partner Bundle Interaktions-Reading mit Alex", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/partner-bundle-mit-alex/payment?pid=63082&prid=9532");
+
+redirectAppointmentType("Codes of Business Reading mit Alex", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/codes-of-business-reading-mit-alex/payment?pid=63082&prid=9532");
+
+redirectAppointmentType("Codes of Business Reading mit Kristin", "https://elopage.com/epl/Ngw-gzph9xTGpyJwCqXx");
+
+redirectAppointmentType("Codes of Business Reading mit Uli", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/codes-of-business-reading-mit-uli/payment?pid=63631&prid=9628");
+
+redirectAppointmentType("Codes of Business Reading mit Katharina", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/codes-of-business-reading-mit-katharina/payment?pid=66698&prid=10051");
+
+redirectAppointmentType("Codes of Business Interaktions-Reading mit Kristin", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/codes-of-business-x2-mit-kristin/payment?pid=31516&prid=9499");
+
+redirectAppointmentType("Codes of Business Mentoring mit Kristin", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/business-mentoring-mit-kristin/payment?pid=31516&prid=9499");
+
+redirectAppointmentType("Codes of Business Mentoring mit Katharina", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/business-mentoring-mit-katharina/payment?pid=66698&prid=10051");
+
+redirectAppointmentType("Branding by Design Session mit Julia", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/business-branding-mit-julia/payment?pid=63075&prid=9531");
+
+redirectAppointmentType("Branding by Design Session mit Janina", "https://shop.humandesign-mentoring.com/s/humandesign-mentoring/branding-session-mit-janina/payment?pid=66716&prid=10054");
